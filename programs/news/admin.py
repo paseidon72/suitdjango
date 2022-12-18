814a57ee8072c3ce0f033cdc3375e1560f6194c4
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-12-18 16:02:00 +0200

    suit

--- programs/news/admin.py
+++ programs/news/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import Newshome
+#
+# admin.site.register(Newshome)
 
 # Register your models here.
--- programs/news/models.py
+++ programs/news/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class Artiles(models.Model):
+
+class Newshome(models.Model):
     title = models.CharField('Название', max_length=50)
     anons = models.CharField('Анонс', max_length=250)
     full_text = models.TextField('Статья')
@@ -9,4 +10,9 @@ class Artiles(models.Model):
 
     def __str__(self):
         return self.title
+
+
+    # class Meta:
+    #     verbose_name = 'Новость'
+    #     verbose_name_plural = 'Новости'
 # Create your models here.
